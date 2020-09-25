b1592fa52824438a8c733b702b790a218e554bb3
Ayodeji Moshood <amoshood@fczmedia.com>
2020-09-25 01:36:08 +0100

    back up before pull from fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Jumbotron, Container, Row, Col, Image, Breadcrumb, Button, Card } from 'react-bootstrap';
+import {
+    Jumbotron,
+    Container,
+    Row,
+    Col,
+    Image,
+    Breadcrumb,
+    Button,
+    Card,
+} from 'react-bootstrap';
 // import AboutSection from '../../AboutSection/AboutSections';
 // import ButtonGroup from '../../ButtonGroup/ButtonGroups'
 // import FormCardSection from '../../FormCardSection/FormCardSections'
 // import SearchTabFormSection from '../../SearchTabFormSection/SearchTabFormSections';
-import ProfileForm from './ProfileForm';
+import ProfileCard from './ProfileCard';
+import ProfileLeftCard from './ProfileLeftCard';
 // import BannerSection from '../BannerSection/BannerSections';
 // import InsuranceThumbnailSection from '../InsuranceThumbnailSection/InsuranceThumbnailSections'
 
@@ -12,9 +22,27 @@ class Profile extends Component {
     render() {
         return (
             <div>
-                
-                <ProfileForm />
                 <br/>
+                <br/>
+                <br/>
+                <Container className="" style={{ maxWidth: '100%' }}>
+                    {/* <MultiStep /> */}
+                    <Col lg={12}>
+                        <Row>
+                            <Col md={8}>
+                                <Card>
+                                    <ProfileCard />
+                                </Card>
+                            </Col>
+
+                            <Col md={4}>
+                                <ProfileLeftCard/>
+                                {/* <br/>
+                                <TicketCard2/> */}
+                            </Col>
+                        </Row>
+                    </Col>
+                </Container>
             </div>
         );
     }
---
+++ src/components/Profile/ProfileLeftCard.jsx
@@ -0,0 +1,54 @@
+
+
+import React, { Component } from 'react';
+import { Jumbotron, Container, Row, Col, Image, Button, Card } from 'react-bootstrap';
+// import bagimg from '../../assets/img/icons/bag.png'
+// import Switch from 'react-switch'
+import airpeaceimg from '../../assets/img/icons/flightbrands/airpeace.png'
+
+class ProfileLeftCard extends Component {
+    
+    render() {
+        return (
+            <div>
+                <Card>
+                    <div style={{ backgroundColor: '#ffffff', padding: '20px', border: 'none', borderRadius: '50px'}}>
+                        <p style={{color: '#043f7c'}}> <b>My Profile</b> </p>
+                        <hr style={{color: '#043f7c'}}/>
+                        <Card body style={{marginLeft: '-20px', marginRight: '-20px', borderRadius: 'initial', border: 'none'}} >
+                            <Row >
+                                <Col md='12'>
+                                    <h6 style={{color: '#043f7c'}}>Account Information</h6>
+                                    <br/>
+                                    <h6 style={{color: '#043f7c'}}>Delivery Address</h6>
+                                </Col>
+                            </Row>
+                        </Card>
+                        <p style={{color: '#043f7c'}}> <b>My Orders</b> </p>
+                        <hr style={{color: '#043f7c'}}/>
+                        <Card body style={{marginLeft: '-20px', marginRight: '-20px', borderRadius: 'initial', border: 'none'}} >
+                            <Row >
+                                <Col md='12'>
+                                    <h6 style={{color: '#043f7c'}}>Account Information</h6>
+                                    <br/>
+                                    <h6 style={{color: '#043f7c'}}>Delivery Address</h6>
+                                </Col>
+                            </Row>
+                        </Card>
+                        <p style={{color: '#043f7c'}}> <b>My Wallet</b> </p>
+                        <hr style={{color: '#043f7c'}}/>
+                        <Card body style={{marginLeft: '-20px', marginRight: '-20px', borderRadius: 'initial', border: 'none'}} >
+                            <Row >
+                                <Col md='12'>
+                                    <h6 style={{color: '#043f7c'}}>Wallet</h6>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </div>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default ProfileLeftCard;
